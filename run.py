4d2f8ba46ad454954bb2949a97f872e101a545ab
Gabriel Vieira <gabrielcnt96@gmail.com>
2025-11-06 21:34:23 -0300

    feat: rota index finalizada

--- run.py
+++ run.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 from app.routes import form_bp
 from app.database import init_db, db
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(form_bp, url_prefix='/form')
+
+CORS(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 init_db(app)
 
